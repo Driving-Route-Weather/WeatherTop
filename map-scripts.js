acf594e1f760bdac9f2c9ae0001f4fe78335842a
unknown <jaredaswensen@gmail.com>
2021-03-11 16:50:19 -0700

    Retrieve a list of city weathers

--- map-scripts.js
+++ map-scripts.js
@@ -2,7 +2,10 @@ let map;
 var directionsService;
 var directionsRenderer;
 var geocoder;
-var API_KEY = '753ad43c3b0a6502b5b443cab998a11e';
+
+// change this to adjust the number of cities/locations sampled for weather data
+var CITIES_LENGTH = 5;
+var WEATHER_API_KEY = '753ad43c3b0a6502b5b443cab998a11e';
 const weatherIntervals = {
     CURRENT: "current",
     MINUTELY: "minutely",
@@ -20,8 +23,6 @@ function initMap() {
         zoom: 8,
     });
     directionsRenderer.setMap(map);
-
-    getWeather(0,0,weatherIntervals.CURRENT);
 }
 
 function calcRoute(event) {
@@ -46,27 +47,93 @@ function calcRoute(event) {
         if (status == 'OK') {
             console.log(result);
             directionsRenderer.setDirections(result);
+            step = Math.floor(result.routes[0].overview_path.length / CITIES_LENGTH);
+            for (i=1; i < CITIES_LENGTH; i++){
+                latlon = result.routes[0].overview_path[i*step];
+                duration = getWeatherFromLatLon(event, latlon);
+            }
+            console.log(result.routes[0].legs[0].end_location);
+            getWeatherPrep(result, result.routes[0].legs[0].end_location);
         }
     });
 }
 
-function getWeather(lat, lon, interval) {
-    var intervalsToExclude = [weatherIntervals.CURRENT, weatherIntervals.MINUTELY, weatherIntervals.HOURLY, weatherIntervals.DAILY]
-
-    const index = intervalsToExclude.indexOf(interval);
-    if (index > -1) {
-        intervalsToExclude.splice(index, 1);
+function getWeatherFromLatLon(event, latlon) {
+    event.preventDefault();
+    var origin = document.getElementById('origin').value;
+    var destination = latlon;
+    var departTime = document.getElementById('start-time').value;
+    if (departTime) {
+        departTime = new Date(departTime);
+    } else {
+        departTime = new Date();
     }
+    var request = {
+        origin: origin,
+        destination: destination,
+        drivingOptions: {
+            departureTime: departTime,
+        },
+        travelMode: 'DRIVING',
+    };
+    directionsService.route(request, function (result, status) {
+        if (status == 'OK') {
+            getWeatherPrep(result, latlon);
+        }
+    });
+}
 
-    var fullURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${intervalsToExclude}&appid=${API_KEY}`;
-    //console.log(fullURL);
+function getPoliticalTypes(address) {
+    return address.types.includes("political");
+}
+
+// this is not working because it is not free (╯°□°）╯︵ ┻━┻
+// this could have been a better way to turn latlon objects into city names
+function getReverseGeocoding(latlon) {
+    var fullURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlon.lat()},${latlon.lng()}&key=AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE`;
     fetch(fullURL)
     .then(function(response) {
       return response.json();
     }).then(function(json) {
-      //console.log(json);
-      return json;
+      console.log(json);
+      location = json.address_components.filter(getPoliticalTypes)[0];
+      return location;
     });
+}
+
+function getWeatherPrep(routeResult, latlon) {
+    var duration_result = routeResult.routes[0].legs[0].duration.text;
+    var city_result = routeResult.routes[0].legs[0].end_address;
+    //the following comment is an option if we can figure out a free version
+    //work around is calling directionsService.route multiple times 
+    //to get duration times and end addresses
+    //city = getReverseGeocoding(latlon);
+    getWeather(latlon, duration_result, city_result);
+}
+
+function getWeather(latlon, duration, city) {
+    var intervalsToExclude = []
+    // Uncomment to exclude intervals
+    // var intervalsToExclude = [weatherIntervals.CURRENT, weatherIntervals.MINUTELY, weatherIntervals.HOURLY, weatherIntervals.DAILY]
+    // const index = intervalsToExclude.indexOf(interval);
+    // if (index > -1) {
+    //     intervalsToExclude.splice(index, 1);
+    // }
+
+    var fullURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latlon.lat()}&lon=${latlon.lng()}&exclude=${intervalsToExclude}&appid=${WEATHER_API_KEY}`;
+    fetch(fullURL)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json["duration"] = duration;
+      json["city"] = city;
+      onWeatherRecieved(json);
+    });
+}
+
+// callback for each individual weather api call
+function onWeatherRecieved(response) {
+    console.log(response);
 }
 
 window.onload = () => {
